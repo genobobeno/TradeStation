eb62cdc81cd7371f4b4c9c88e64984b588769d79
eugene.quickreaction@gmail.com <eugene.quickreaction@gmail.com>
2020-10-27 17:08:31 -0400

    Better Initialize File

--- Initialize.R
+++ Initialize.R
@@ -9,24 +9,28 @@ if (Sys.getenv("COMPUTERNAME")=="D2X8CJR2") {
   setwd("C:/Users/Eugene/Nextcloud/Documents/Trading/TradeStation/")
 }
 options(stringsAsFactors = FALSE,scipen=999)
-library(ggplot2)
-library(xts)
-#library(dygraphs)
-library("RCurl")
-library("jsonlite")
-library("httr")
-library("stringr")
-library("plotly")
-library("TTR")
-library("quantmod")
-library("psych")
+
+PACK = c("ggplot2","xts","RCurl","jsonlite","httr","stringr","plotly","TTR","quantmod","psych")
+
+### Install packages not already installed ###
+for (i in 1:length(PACK)){
+  if(PACK[i] %in% rownames(installed.packages()) == FALSE) {
+    print(PACK[i])
+    if (isOpen(url(description = "https://www.google.com"))) install.packages(PACK[i],dependencies = TRUE)}
+}
+
+lapply(PACK, require, character.only = TRUE)
+
 source("ROandaAPIFunctions.R")
 source("PullPriceData.R")
 source("DataCleanup.R")
+source("PriceETL.R")
 source("Analyses.R")
 source("CountFunctions.R")
 source("ChartPrices.R")
 source("AddIndicators.R")
 source("CombiningData.R")
 source("ModelPreProcess.R")
+source("WeeklyDataArchive.R")
+
 
